fbfecff13e37ee070edc18186ff1d16533644331
Bridgette <turner.bridgette@yahoo.com>
2020-12-05 19:10:55 -0700

    ready for deploy

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const Pool = require("pg").Pool;
 require('dotenv').config()
+const config = require('../config');
 
 const pool = new Pool({
   host: "ec2-54-85-13-135.compute-1.amazonaws.com",//"localhost",
   database: "d5vj2k1t7ho7tg",//"phonebook",
   user: "oqiwieyditarhs",//"bridgette",
   port: 5432,
-  password: process.env.jwtSecret,
+  password: config.jwtSecret,
   ssl: true,
 });
 
