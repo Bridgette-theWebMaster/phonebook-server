491753a835a14b00f7f8dfe42e05d85e6fc89125
Bridgette <turner.bridgette@yahoo.com>
2020-12-06 08:32:56 -0700

    ready for deploy

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   database: "d5vj2k1t7ho7tg",//"phonebook",
   user: "oqiwieyditarhs",//"bridgette",
   port: 5432,
-  password: config.jwtSecret,//"045b3aa90fd9d98874be76d8ae6c34008d8f2b7920509c156bd97abf2af7ab67",
+  password: "045b3aa90fd9d98874be76d8ae6c34008d8f2b7920509c156bd97abf2af7ab67",
 });
 
 module.exports = pool;
